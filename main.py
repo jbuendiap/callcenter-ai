a3245df4af19756dd5608b9dc9266a1ee36357f8
jbuendiap <jbuendiap256@gmail.com>
2026-03-30 20:10:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,20 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Call Center AI funcionando"}
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Call Center AI funcionando"}
+
+@app.post("/call")
+async def receive_call(request: Request):
+    data = await request.json()
+    
+    print("Llamada recibida:", data)
+
+    return {
+        "response": "Hola, gracias por llamar al hotel. ¿En qué puedo ayudarte?"
+    }
